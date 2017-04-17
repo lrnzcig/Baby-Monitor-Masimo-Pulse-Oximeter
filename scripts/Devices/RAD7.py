86da2ba1bc3d6c214a78590f60eec1e1968263b2
Lorenzo Rubio <lrnzcig@gmail.com>
2017-04-17 20:47:22 +0200

    Improve exception handling
    Sometimes the python program would stop. A few silly mistakes.

--- scripts/Devices/RAD7.py
+++ scripts/Devices/RAD7.py
@@ -23,42 +23,46 @@ def create_event_from_output(line):
             parsed_string[0] = parsed_string[0][1:]
 
         #parse date time from first 2 parsed items
+        date = None
         try:
-            date = datetime.strptime(parsed_string[0] + " " + parsed_string[1], "%m/%d/%y %H:%M:%S")
-        except ValueError:
-            logger.error("Cannot convert '" + parsed_string[0] + " " + parsed_string[1] + "' to date")
+            date = datetime.strptime(parsed_string[0].replace('\x00', "") + " " + parsed_string[1].replace('\x00', ""),
+                "%m/%d/%y %H:%M:%S")
+        except Exception as e:
+            logger.error("Cannot convert '" + parsed_string[0] + " " + parsed_string[1] + "' to date", e)
 
 		#get serial number
+        serial_number = None
         try:
             serial_number = parsed_string[2].split("=")[1]
-        except IndexError:
-            logger.error("Index error: '" + parsed_string[2] + "'")
+        except Exception as e:
+            logger.error("Index error: '" + parsed_string[2] + "'", e)
 
         #try and parse SPO2, this will fail when the value is "--%"
         spo2 = -1
         try:
             spo2 = int(parsed_string[3].split("=")[1].replace("%", ""))
-        except (ValueError, IndexError):
-            logger.error("Unknown conversion errror spo2: '"+ parsed_string[3] + "'")
+        except Exception as e:
+            logger.error("Unknown conversion errror spo2: '"+ parsed_string[3] + "'", e)
 
 		#try and parse BPM, this will fail when the value is "--%""
         beat_per_minute = -1
         try:
             beat_per_minute = int(parsed_string[4].split("=")[1])
-        except (ValueError, IndexError):
-            logger.error("Unable to parse BPM: '" + parsed_string[4] + "'")
+        except Exception as e:
+            logger.error("Unable to parse BPM: '" + parsed_string[4] + "'", e)
 
 		#try and parse PI, this will fail when the value is "--%"
         perfusion_index = -1
         try:
             perfusion_index = float(parsed_string[5].split("=")[1].replace("%", ""))
-        except (ValueError, IndexError):
-            logger.error("Unable to parse PI: '" + parsed_string[5] + "'")
+        except Exception as e:
+            logger.error("Unable to parse PI: '" + parsed_string[5] + "'", e)
 
+        alarm = None
         try:
-            alarm = parsed_string[10].split("=")[1]
-        except IndexError:
-            logger.error("Unable to parse alarm: '" + parsed_string[10] + "'")
+            alarm = parsed_string[14].split("=")[1]
+        except Exception as e:
+            logger.error("Unable to parse alarm: '" + parsed_string[10] + "'", e)
 
         doc = {
             "date" : date,
